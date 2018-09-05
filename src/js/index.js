f2cb95fe3e91caa1cd20884e61d25e5e84eadfb1
Zeb Pykosz <zeb.apy@gmail.com>
2018-09-05 08:50:46 -0400

    progress on midd-header

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
+import focusWithin from 'focus-within';
+
 import './headroom';
 import './toggler';
 import './video';
 import './responsive-table';
 import './events-datepicker';
 import './audio-player';
+
+focusWithin(document);
