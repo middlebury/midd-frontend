65ad41fe4fbcfc4d871c17202834c2971d52534a
Zeb Pykosz <zeb.apy@gmail.com>
2018-10-18 10:32:25 -0400

    add some comments to main imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,22 @@
+// polyfill nodelist foreach so you can
+// use document.querySelectorAll('.my-elems').forEach() in IE
 import 'polyfill-nodelist-foreach';
+
+// polyfill intersection observer for ie and safari
+// used in menu-spy.js and triggering chart animations when they scroll into view
 import 'intersection-observer';
-import stickybits from 'stickybits';
-import focusWithin from 'focus-within';
 
-import './headroom';
-import './toggler';
-import './video';
-import './digest';
-import './responsive-table';
-import './events-datepicker';
-import './audio-player';
+import stickybits from 'stickybits'; // sticky element support for ie11
+import focusWithin from 'focus-within'; // polyfill :focus-within for tab accessible menus
 
-import './offices';
+import './headroom'; // sticky site headers
+import './toggler'; // toggler util used in transcript toggle, accordions, and more
+import './video'; // for lazy loading iframes until a video thumbnail is clicked
+import './digest'; // table of contents creator
+import './responsive-table'; // adds data-th attributes for responsive tables
+import './events-datepicker'; // make the events datepicker accessible
+import './audio-player'; // custom Preact audio player
+import './offices'; // middlebury.edu/office homepage script for filtering items shown
 
 focusWithin(document);
 
