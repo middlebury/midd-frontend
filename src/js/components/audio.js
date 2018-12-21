bfc87e67b1f7697d6e5bc725898eed24482b84c9
Zeb Pykosz <zeb.apy@gmail.com>
2018-12-21 10:25:47 -0500

    use mute/unmute icons for audio player

--- src/js/components/audio.js
+++ src/js/components/audio.js
@@ -123,8 +123,14 @@ class AudioPlayer extends Component {
           onTrackClick={this.handleTrackClick}
         />
         <div class="audio__mute">
-          <button class="button button--sm" onClick={this.mute}>
-            {muted ? 'Unmute' : 'Mute'}
+          <button
+            class="button px-3"
+            onClick={this.mute}
+            aria-label={muted ? 'Unmute' : 'Mute'}
+          >
+            <svg className="icon" aria-hidden="true">
+              <use xlinkHref={`#icon-volume-${muted ? 'mute' : 'up'}`} />
+            </svg>
           </button>
         </div>
       </div>
