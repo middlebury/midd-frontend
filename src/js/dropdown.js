47a0799165cde623ac7031c5429dab793cb1e414
Zeb Pykosz <zeb.apy@gmail.com>
2019-04-05 16:09:32 -0400

    add school explorer component

---
+++ src/js/dropdown.js
@@ -0,0 +1,80 @@
+import { $, $$, on, off } from './utils/dom';
+
+class Dropdown {
+  constructor(elem) {
+    this.elem = elem;
+
+    this.btn = $('[data-dropdown-button]', elem);
+    this.menu = $('[data-dropdown-menu]', elem);
+
+    this.activeClass = 'is-active';
+    this.isOpen = false;
+
+    this.init();
+  }
+
+  init() {
+    this.addListeners();
+  }
+
+  addListeners() {
+    on(window, 'click', this.handleWindowClick);
+    on(window, 'touchstart', this.handleWindowClick);
+
+    on(this.elem, 'keyup', this.handleElemKeyup);
+
+    on(this.btn, 'click', this.toggle);
+  }
+
+  handleElemKeyup = e => {
+    if (e.keyCode === 27) {
+      this.hide();
+      this.focusBtn();
+    }
+  };
+
+  focusBtn() {
+    this.btn.focus();
+  }
+
+  handleWindowClick = e => {
+    const node = this.elem;
+
+    if (event.target !== node && !node.contains(event.target)) {
+      this.hide();
+    }
+  };
+
+  toggle = () => {
+    if (this.isOpen) {
+      this.hide();
+    } else {
+      this.show();
+
+      this.focusFirst();
+    }
+  };
+
+  focusFirst() {
+    const el = $('a', this.menu);
+    el.focus();
+  }
+
+  hide() {
+    this.elem.classList.remove(this.activeClass);
+    this.btn.setAttribute('aria-expanded', 'false');
+    this.isOpen = false;
+  }
+
+  show() {
+    this.elem.classList.add(this.activeClass);
+    this.btn.setAttribute('aria-expanded', 'true');
+    this.isOpen = true;
+  }
+}
+
+const elems = $$('[data-dropdown]');
+
+elems.forEach(elem => new Dropdown(elem));
+
+export default Dropdown;
--- src/js/index.js
+++ src/js/index.js
@@ -24,6 +24,7 @@ import './offices'; // middlebury.edu/office homepage script for filtering items
 import './slideshow'; // swiperjs slideshows
 import './mover'; // swiperjs slideshows
 import './card-carousel';
+import './dropdown';
 
 focusWithin(document);
 
