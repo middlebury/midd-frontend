80d1d0cd995044816d2484e6983097d923c037a5
Zeb Pykosz <zeb.apy@gmail.com>
2019-04-29 12:00:21 -0400

    use built in disableScroll setting from micromodal

--- src/js/drawer.js
+++ src/js/drawer.js
@@ -1,12 +1,5 @@
 import Micromodal from 'micromodal';
 
-const toggleBodyClass = () => document.body.classList.toggle('has-drawer');
-
 Micromodal.init({
-  onShow() {
-    toggleBodyClass();
-  },
-  onClose() {
-    toggleBodyClass();
-  }
+  disableScroll: true
 });
