bf80a3965048b7c164a820cefe24228a3002bdea
Zeb Pykosz <zeb.apy@gmail.com>
2019-09-03 14:13:13 -0400

    Create autoheight-iframe.js

---
+++ docs/src/components/autoheight-iframe.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const AutoheightIframe = props => {
+  const [height, setHeight] = React.useState(200);
+
+  const sizeIframe = event => {
+    console.log(event);
+    setHeight(event.target.contentWindow.document.body.scrollHeight);
+  };
+
+  return (
+    <iframe
+      {...props}
+      onLoad={sizeIframe}
+      style={{
+        width: '100%',
+        border: 0,
+        height,
+      }}
+    />
+  );
+};
