46ff0c31f93f170a7356157aad8cd4d26618ecf5
Zeb Pykosz <zeb.apy@gmail.com>
2019-09-18 12:15:12 -0400

    remove css comment

--- docs/src/components/breakpoint-sizer.js
+++ docs/src/components/breakpoint-sizer.js
@@ -29,11 +29,6 @@ const Btn = p => (
   />
 );
 
-//       color: var(--primary);
-//       border-color: var(--gray1);
-//       border-bottom-color: white;
-// `;
-
 const BreakpointSizer = ({
   breakpoints = defaultBreakpoints,
   defaultSize = 3,
