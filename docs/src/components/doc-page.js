c9fb385642bda712c19c7ba1e422bed60d09a51a
Zeb Pykosz <zeb.apy@gmail.com>
2019-10-09 11:06:32 -0400

    move mdx components to file

--- docs/src/components/doc-page.js
+++ docs/src/components/doc-page.js
@@ -1,85 +1,10 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import React from 'react';
-import { Link, graphql } from 'gatsby';
 import { MDXProvider } from '@mdx-js/react';
-import Prism from '@theme-ui/prism';
 
 import SEO from './seo';
-import ColorGrid from './color-grid';
-import Example from './doc-example';
-
-const h2 = props => (
-  <h2 sx={{ mt: 9, mb: 2, lineHeight: 'heading' }} {...props} />
-);
-
-const components = {
-  a: props => (
-    <a
-      {...props}
-      sx={{
-        color: 'sky',
-      }}
-    />
-  ),
-  pre: props => props.children,
-  Example,
-  code: props =>
-    props.example ? <Example {...props} /> : <Prism {...props} />,
-  ColorGrid,
-  h1: h2,
-  h2,
-  ol: props => <ol sx={{ listStyle: 'decimal', mb: 4 }} {...props} />,
-  ul: props => <ul sx={{ listStyle: 'disc', mb: 4 }} {...props} />,
-  li: props => <li sx={{ mb: 2, ml: 3 }} {...props} />,
-  p: props => <p sx={{ mb: 4 }} {...props} />,
-  table: props => (
-    <table
-      sx={{
-        bg: 'white',
-        tableLayout: 'fixed',
-        width: '100%',
-        border: 0,
-        borderCollapse: 'collapse',
-        mb: 4,
-      }}
-      {...props}
-    />
-  ),
-  tr: props => (
-    <tr
-      sx={{
-        ':nth-child(even)': {
-          bg: '#f2f2f2',
-        },
-      }}
-      {...props}
-    />
-  ),
-  th: props => (
-    <th
-      sx={{
-        p: 2,
-        fontSize: 1,
-        fontWeight: 600,
-        textAlign: 'left',
-        bg: '#f2f2f2',
-      }}
-      {...props}
-    />
-  ),
-  td: props => (
-    <td
-      sx={{
-        p: 2,
-        fontSize: 2,
-        // border: '1px solid',
-        // borderColor: 'gray1',
-      }}
-      {...props}
-    />
-  ),
-};
+import { components } from './mdx-components';
 
 class DefaultPageLayout extends React.Component {
   render() {
---
+++ docs/src/components/mdx-components.js
@@ -0,0 +1,78 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import Prism from '@theme-ui/prism';
+
+import ColorGrid from './color-grid';
+import Example from './doc-example';
+
+const h2 = props => (
+  <h2 sx={{ mt: 9, mb: 2, lineHeight: 'heading' }} {...props} />
+);
+
+export const components = {
+  Example,
+  ColorGrid,
+  a: props => (
+    <a
+      {...props}
+      sx={{
+        color: 'sky',
+      }}
+    />
+  ),
+  pre: props => props.children,
+  code: props =>
+    props.example ? <Example {...props} /> : <Prism {...props} />,
+  h1: h2,
+  h2,
+  ol: props => <ol sx={{ listStyle: 'decimal', mb: 4 }} {...props} />,
+  ul: props => <ul sx={{ listStyle: 'disc', mb: 4 }} {...props} />,
+  li: props => <li sx={{ mb: 2, ml: 3 }} {...props} />,
+  p: props => <p sx={{ mb: 4 }} {...props} />,
+  table: props => (
+    <table
+      sx={{
+        bg: 'white',
+        tableLayout: 'fixed',
+        width: '100%',
+        border: 0,
+        borderCollapse: 'collapse',
+        mb: 4,
+      }}
+      {...props}
+    />
+  ),
+  tr: props => (
+    <tr
+      sx={{
+        ':nth-child(even)': {
+          bg: '#f2f2f2',
+        },
+      }}
+      {...props}
+    />
+  ),
+  th: props => (
+    <th
+      sx={{
+        p: 2,
+        fontSize: 1,
+        fontWeight: 600,
+        textAlign: 'left',
+        bg: '#f2f2f2',
+      }}
+      {...props}
+    />
+  ),
+  td: props => (
+    <td
+      sx={{
+        p: 2,
+        fontSize: 2,
+        // border: '1px solid',
+        // borderColor: 'gray1',
+      }}
+      {...props}
+    />
+  ),
+};
