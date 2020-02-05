f5648f2173b5e78e21b9d9074bb65b08b3b6e6a2
Zeb Pykosz <zeb.apy@gmail.com>
2020-02-05 09:57:42 -0500

    fix module import name

--- rollup.js
+++ rollup.js
@@ -7,7 +7,7 @@ const commonJS = require('@rollup/plugin-commonjs');
 const { uglify } = require('rollup-plugin-uglify');
 const sizes = require('rollup-plugin-sizes');
 const filesize = require('rollup-plugin-filesize');
-const replace = require('rollup-plugin-replace');
+const replace = require('@rollup/plugin-replace');
 
 const production = process.env.NODE_ENV === 'production';
 
