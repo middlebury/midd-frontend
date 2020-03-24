635f82a28a8e65716bfcec7b7b72b96292398528
Zeb Pykosz <zeb.apy@gmail.com>
2020-03-24 16:57:15 -0400

    add smooth scrolling animation to top top button

--- src/js/to-top-btn.js
+++ src/js/to-top-btn.js
@@ -1,9 +1,12 @@
 import { $ } from './utils/dom';
 import onscroll from './utils/onscroll';
 
+import SmoothScroll from './smooth-scroll';
+
 const btn = $('.js-to-top-btn');
 btn.hidden = true;
 
+new SmoothScroll('.js-to-top-btn');
 
 // Set threshold to 4x client height so we don't
 // show the button on short pages.
