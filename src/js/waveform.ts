6bf48a8a5e425607276a98e8d1741be6b8220cf3
Ian McBride <imcbride@middlebury.edu>
2022-06-27 15:10:54 -0400

    Add naive implementation of an event listener to reclamp text when the user clicks on the waveform.

--- src/js/waveform.ts
+++ src/js/waveform.ts
@@ -1,3 +1,5 @@
 import Superclamp from 'superclamp';
 
 Superclamp.register(document.querySelectorAll('.waveform__event-card__content--text'));
+
+window.addEventListener('click', Superclamp.reclampAll);
