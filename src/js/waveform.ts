86ceb09e342de3cb17ebfb78e6a783876d588f28
poojagunturu96 <pgunturu@middlebury.edu>
2022-07-12 14:33:24 -0500

    Moved contents of waveform.ts to shifting-slider.tsx to keep all waveform js code in one file

--- src/js/waveform.ts
+++
@@ -1,8 +0,0 @@
-import Superclamp from 'superclamp';
-
-Superclamp.register(
-  document.querySelectorAll('.waveform__event-card__content--text')
-);
-
-const cards = document.querySelectorAll('.waveform__list-item');
-cards.forEach((elem) => elem.addEventListener('click', Superclamp.reclampAll));
