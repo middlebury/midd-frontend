88b222ea332eea568968f11b4c39b9f72bf8bc7e
poojagunturu96 <pgunturu@middlebury.edu>
2022-11-01 11:35:13 -0500

    Initial code for the institutional homepage

--- src/js/journey.ts
+++ src/js/journey.ts
@@ -1,7 +1,6 @@
 // https://www.visualcinnamon.com/2016/01/animating-dashed-line-d3
 
 // Code not used in production yet. Remove eslint-disable when we ship it.
-/* eslint-disable */
 
 const pathEl = document.querySelector('.journey-line--desktop path');
 // @ts-expect-error ts-migrate(2531) FIXME: Object is possibly 'null'.
@@ -10,21 +9,17 @@ const dashing = '2, 2';
 
 const dashLength = dashing
   .split(/[\s,]/)
-  .map(a => parseFloat(a) || 0)
+  .map((a) => parseFloat(a) || 0)
   .reduce((a, b) => a + b, 0);
 
 const dashCount = Math.ceil(totalLength / dashLength);
 const newDashes = new Array(dashCount).join(dashing + ' ');
 const dashArray = newDashes + ' 0, ' + totalLength;
 
-// @ts-expect-error ts-migrate(2578) FIXME: Unused '@ts-expect-error' directive.
-// @ts-expect-error ts-migrate(2531) FIXME: Object is possibly 'null'.
 pathEl.setAttribute('stroke-dashoffset', totalLength);
-// @ts-expect-error ts-migrate(2578) FIXME: Unused '@ts-expect-error' directive.
-// @ts-expect-error ts-migrate(2531) FIXME: Object is possibly 'null'.
+
 pathEl.setAttribute('stroke-dasharray', dashArray);
 
-// @ts-expect-error ts-migrate(2304) FIXME: Cannot find name 'anime'.
 const journeyLine = anime({
   targets: pathEl,
   strokeDashoffset: [totalLength, 0],
@@ -39,7 +34,7 @@ const journeyLine = anime({
 
 const sections = document.querySelectorAll('.js-journey-section');
 
-sections.forEach(elem => {
+sections.forEach((elem) => {
   const io = new IntersectionObserver(handleIntersection, {
     threshold: [0.5] // add class when elem is half in view
   });
@@ -59,7 +54,7 @@ sections.forEach(elem => {
 
           setTimeout(() => {
             journeyLine.pause();
-          // @ts-expect-error ts-migrate(2769) FIXME: Type 'null' is not assignable to type 'number | un... Remove this comment to see the full error message
+            // @ts-expect-error ts-migrate(2769) FIXME: Type 'null' is not assignable to type 'number | un... Remove this comment to see the full error message
           }, elem.getAttribute('data-line-duration'));
         }
 
