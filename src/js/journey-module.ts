16445ec8278d293e9ecb1df9fd6305acafb82792
poojagunturu96 <pgunturu@middlebury.edu>
2023-03-10 15:18:41 -0600

    Added a file for journey code for institutional homepage, updated layout.twig to have functionality to add additional scripts

---
+++ src/js/journey-module.ts
@@ -0,0 +1,6 @@
+import './headroom'; // sticky site headers
+import './toggler'; // toggler util used in transcript toggle, accordions, and more
+import './video-controls';
+import './journey';
+import './panel-snap';
+import './journey-swiper';
