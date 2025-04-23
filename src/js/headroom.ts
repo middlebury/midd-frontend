368cce13a27197ad1fbd4354f2c86f91e717fc54
poojagunturu96 <pgunturu@middlebury.edu>
2025-04-23 17:43:44 -0500

    intial code for sticky header in curriculum component

--- src/js/headroom.ts
+++ src/js/headroom.ts
@@ -1,36 +1,38 @@
 import Headroom from 'headroom.js';
 
-import { $ } from './utils/dom';
+import { $, $$ } from './utils/dom';
 
-const headerElem = $('.js-headroom');
+const headerElems = $$('.js-headroom');
 
 let offset;
 
-if (headerElem && !$('#toolbar-administration')) {
-  offset = headerElem.offsetHeight;
+if (headerElems && !$('#toolbar-administration')) {
+  headerElems.forEach((headerElem) => {
+    offset = headerElem.offsetHeight;
 
-  const headerInstance = new Headroom(headerElem, {
-    offset,
-    tolerance: {
-      up: 10,
-      down: 0
-    },
-    // $('#midd-journey-overlay') is for the modal header on the institutional homepage,
-    // If it is present, we make the modal div the scrolling context
-    scroller: $('#midd-journey-overlay') || window
+    const headerInstance = new Headroom(headerElem, {
+      offset,
+      tolerance: {
+        up: 10,
+        down: 0
+      },
+      // $('#midd-journey-overlay') is for the modal header on the institutional homepage,
+      // If it is present, we make the modal div the scrolling context
+      scroller: $('#midd-journey-overlay') || window
+    });
+
+    document.body.style.paddingTop = offset + 'px';
+
+    headerInstance.init();
+
+    // hide sticky header if user navigated to page via anchor hash link
+    // fixes https://github.com/middlebury/midd-frontend/issues/241
+    if (window.location.hash && !$('#midd-journey-overlay')) {
+      // unpin after a delay so user is given some context as
+      // to where they are via header
+      setTimeout(function () {
+        headerInstance.unpin();
+      }, 1000);
+    }
   });
-
-  document.body.style.paddingTop = offset + 'px';
-
-  headerInstance.init();
-
-  // hide sticky header if user navigated to page via anchor hash link
-  // fixes https://github.com/middlebury/midd-frontend/issues/241
-  if (window.location.hash && !$('#midd-journey-overlay')) {
-    // unpin after a delay so user is given some context as
-    // to where they are via header
-    setTimeout(function () {
-      headerInstance.unpin();
-    }, 1000);
-  }
 }
