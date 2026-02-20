ad9e68ed444f257ccfb9a0d730a7797806b6fde7
poojagunturu96 <pgunturu@middlebury.edu>
2026-02-20 17:13:52 -0600

    midd stories: update menu code to usability issue

--- src/js/drawer.ts
+++ src/js/drawer.ts
@@ -9,11 +9,5 @@ Micromodal.init({
 Micromodal.init({
   openTrigger: 'data-drawer-midd-hub',
   closeTrigger: 'data-drawer-close',
-  disableScroll: false,
-  onShow: () => {
-    document.body.classList.add('has-toggled-elem');
-  },
-  onClose: () => {
-    document.body.classList.remove('has-toggled-elem');
-  }
+  disableScroll: false
 });
